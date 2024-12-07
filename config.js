dafc5acb8f1cca5e189fd4f6cca35cb8fac719db
Tylor <phantomtylor@gmail.com>
2024-12-08 01:30:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  PREFIX: process.env.PREFIX || "XLICON-V4~",
+  EMAIL: process.env.EMAIL || "tyloromondi@gmail.com",
+  PASS: process.env.PASS || "tylor1234",
+  MESSAGE: process.env.MESSAGE || "*SUCCESSFULLY LINKED XLICON-V4-MD",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
