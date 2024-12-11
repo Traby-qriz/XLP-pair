f1902203375020e55bc509c49eefbdcd204d5551
Tylor <phantomtylor@gmail.com>
2024-12-11 13:19:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "XLICON-V4~",
+  PREFIX: process.env.PREFIX || "XPLOADER~",
   EMAIL: process.env.EMAIL || "tyloromondi@gmail.com",
   PASS: process.env.PASS || "tylor1234",
-  MESSAGE: process.env.MESSAGE || "*XLICON-V4-MD SESSION ID BELOW*",
+  MESSAGE: process.env.MESSAGE || "*XPLOADER BOT SESSION ID BELOW*",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
