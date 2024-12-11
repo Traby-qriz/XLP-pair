c74e27748a190f7a87a0c89c24be1f7ddd4c172d
Tylor <phantomtylor@gmail.com>
2024-12-11 13:21:19 +0300

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 const { File } = require('megajs');
 const fs = require('fs');
 
-var prefix = "XLICON-V4~"; //your prefix same as in config.PREFIX
+var prefix = "XPLOADER~"; //your prefix same as in config.PREFIX
 var output = "./session/"; //path where the creds.json will save
 
 async function saveCreds(id) {
