233a286752d7f6329eb81e256d86ab03dc05815e
Casper tech <146692830+Traby-qriz@users.noreply.github.com>
2025-02-13 13:10:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,29 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "XLICON-V4~",
-  EMAIL: process.env.EMAIL || "tyloromondi@gmail.com",
-  PASS: process.env.PASS || "tylor1234",
-  MESSAGE: process.env.MESSAGE || "*XLICON-V4-MD SESSION ID BELOW*",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+  PREFIX: process.env.PREFIX || "CASPER-TECH~",
+  EMAIL: process.env.EMAIL || "casperqriz@gmail.com",
+  PASS: process.env.PASS || "jm20032000",
+  MESSAGE: process.env.MESSAGE || "
+ *✅𝐬𝐞𝐬𝐬𝐢𝐨𝐧 𝐠𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐩𝐞𝐫𝐟𝐞𝐜𝐭𝐥𝐲✅*
+ ______________________________
+ ╔════◇
+ ║『 𝙲𝙰𝚂𝙿𝙴𝚁-𝚇𝙼𝙳 𝚆𝙰𝚂 𝙰𝙳𝙳𝙴𝙳 𝚃𝙾 𝚈𝙾𝚄𝚁 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 😉 』
+ ║ You've Completed the First Step
+ ║ to Deploy a Whatsapp Bot.
+ ╚══════════════╝
+ ╔═════◇
+ ║ 『❣️ ⋆G⋆E⋆T⋆ H⋆E⋆L⋆P⋆ H⋆E⋆R⋆E⋆ ❣️』
+ ║❇️𝐘𝐨𝐮𝐭𝐮𝐛𝐞: _youtube.com/@casper.tech.254_
+ ║❇️𝐎𝐰𝐧𝐞𝐫: _https://t.me/casper_tech_ke
+ ║✳️𝐑𝐞𝐩𝐨: _https://github.com/Traby-qriz/CASPER-XMD
+ ║✳️𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029VazABxMJZg40sEZBX242/0029VaYauR9ISTkHTj4xvi1l_
+ ║✳️𝐖𝐚𝐆𝐫𝐨𝐮𝐩 : _
+ ║ 😋💝💝💝💝🤪
+ ╚══════════════╝ 
+  𝗖𝗔𝗦𝗣𝗘𝗥-𝗫𝗠𝗗 
+  > 𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝙲𝙰𝚂𝙿𝙴𝚁 𝚃𝙴𝙲𝙷 𝙺𝙴𝙽𝚈𝙰
+ _______________________________
+
+ 
+ Dont Forget To Give Star⭐ To My Repo",
+  IMAGE: process.env.IMAGE || "https://i.ibb.co/G9xgd0h/IMG-20250126-042719.png",
 }
