c6b7627ae579df0a6d92d27a075be3bc95edecb9
Casper tech <146692830+Traby-qriz@users.noreply.github.com>
2025-03-11 15:26:55 +0300

    𝙲𝙰𝚂𝙿𝙴𝚁-𝚃𝙴𝙲𝙷

--- mega.js
+++ mega.js
@@ -23,12 +23,24 @@ var upload = (pth) => {
     return new Promise((resolve, reject) => {
         var myre = `${crypto.randomBytes(5).toString('hex')}${path.extname(pth)}`;
         var storage = new mega.Storage(auth, () => {
-            var Json = require(pth);
-            var Content = Buffer.from(JSON.stringify(Json));
-            var stream = storage.upload({ name: myre, size: Content.length, allowUploadBuffering: true });
-            stream.end(Content);
-            stream.on('complete', (file) => file.link((err, url) => err ? reject(err) : resolve(url)));
-            stream.on('error', (error) => reject(error));
+            try {
+                var Json = require(pth);
+                var Content = Buffer.from(JSON.stringify(Json));
+                var stream = storage.upload({ name: myre, size: Content.length, allowUploadBuffering: true });
+                stream.end(Content);
+                stream.on('complete', (file) => file.link((err, url) => err ? reject(err) : resolve(url)));
+                stream.on('error', (error) => reject(error));
+            } catch (error) {
+                reject(error);
+            }
+        });
+
+        storage.on('error', (error) => {
+            reject(new Error('Storage error: ' + error.message));
+        });
+        
+        storage.on('ready', () => {
+            console.log('Storage is ready');
         });
     });
 };
@@ -36,14 +48,14 @@ var upload = (pth) => {
 module.exports = { upload };
 
 /*
-* Inspired from 
-* https://github.com/Guru322/Express-pairing-code/blob/main/mega.js
-*
-* IN CASE IF YOUR LOOKING FOR MEDIA UPLOAD THEN HERE
-* (and dont wanna go through the docs of megajs then)
-*
-* var size = statSync(filePath).size;
-* var stream = storage.upload({ name: myre, size: size, allowUploadBuffering: true });
-* createReadStream(filePath).pipe(stream);
-* stream.on('complete', (file) => file.link((err, url) => err ? reject(err) : resolve(url)));
-*/
+ * Inspired from 
+ * https://github.com/Guru322/Express-pairing-code/blob/main/mega.js
+ *
+ * IN CASE IF YOUR LOOKING FOR MEDIA UPLOAD THEN HERE
+ * (and dont wanna go through the docs of megajs then)
+ *
+ * var size = statSync(filePath).size;
+ * var stream = storage.upload({ name: myre, size: size, allowUploadBuffering: true });
+ * createReadStream(filePath).pipe(stream);
+ * stream.on('complete', (file) => file.link((err, url) => err ? reject(err) : resolve(url)));
+ */
