f9bd541ceef0b3499c60a972faa2659327851cd3
Casper tech <146692830+Traby-qriz@users.noreply.github.com>
2025-03-11 15:21:35 +0300

    𝙲𝙰𝚂𝙿𝙴𝚁-𝚃𝙴𝙲𝙷

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const {
-   spawn
-} = require('child_process')
-const path = require('path')
+const { spawn } = require('child_process');
+const path = require('path');
 
 function start() {
-   let args = [path.join(__dirname, 'start.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Bot...')
-            p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
+    try {
+        let args = [path.join(__dirname, 'start.js'), ...process.argv.slice(2)];
+        console.log([process.argv[0], ...args].join('\n'));
+        
+        let p = spawn(process.argv[0], args, {
+            stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+        })
+        .on('message', data => {
+            if (data == 'reset') {
+                console.log('Restarting Bot...');
+                p.kill();
+                start();
+            }
+        })
+        .on('exit', code => {
+            console.error('Exited with code:', code);
+            if (code === 0 || code === 1) start();
+        });
+    } catch (error) {
+        console.error('Error starting the process:', error);
+    }
 }
-start()
+
+start();
